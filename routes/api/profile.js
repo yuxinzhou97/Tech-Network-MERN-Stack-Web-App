7ad421c92fe72fe54cc11aa4079aba5a4669bca2
Yuxin Zhou <yuxinzhou1997@gmail.com>
2020-08-30 00:15:11 -0700

    create or update user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
-
+const { check, validationResult } = require("express-validator");
+const normalize = require("normalize-url");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
@@ -25,5 +26,81 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route    POST api/profile
+// @desc     Create or update user profile
+// @access   Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // pull these from request body
+    const {
+      company,
+      location,
+      website,
+      bio,
+      skills,
+      status,
+      githubusername,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+    } = req.body;
+
+    // build profile object
+    const profileFields = {
+      user: req.user.id,
+      company,
+      location,
+      website:
+        website && website !== ""
+          ? normalize(website, { forceHttps: true })
+          : "",
+      bio,
+      skills: Array.isArray(skills)
+        ? skills
+        : skills.split(",").map((skill) => " " + skill.trim()),
+      status,
+      githubusername,
+    };
+
+    // build social object and add to profileFields
+    const socialfields = { youtube, twitter, instagram, linkedin, facebook };
+    for (const [key, value] of Object.entries(socialfields)) {
+      if (value && value.length > 0) {
+        socialfields[key] = normalize(value, { forceHttps: true });
+      }
+    }
+
+    try {
+      // Using upsert option (creates new doc if no match is found):
+      let profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true, upsert: true }
+      );
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+
+    res.send("hello");
+  }
+);
 
 module.exports = router;
