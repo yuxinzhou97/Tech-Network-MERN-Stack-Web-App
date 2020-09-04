35455bccf07cd3a58bcbb3b7f3e28715b25712d6
Yuxin Zhou <yuxinzhou1997@gmail.com>
2020-09-04 16:35:44 -0700

    Update Routes.js

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -7,7 +7,7 @@ import Dashboard from "../dashboard/Dashboard";
 import ProfileForm from "../profile-forms/ProfileForm";
 import AddExperience from "../profile-forms/AddExperience";
 import AddEducation from "../profile-forms/AddEducation";
-// import Profiles from "../profiles/Profiles";
+import Profiles from "../profiles/Profiles";
 // import Profile from "../profile/Profile";
 //import Posts from "../posts/Posts";
 //import Post from "../post/Post";
@@ -21,8 +21,8 @@ const Routes = (props) => {
       <Switch>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
-        {/* <Route exact path="/profiles" component={Profiles} />
-        <Route exact path="/profile/:id" component={Profile} /> */}
+        <Route exact path="/profiles" component={Profiles} />
+        {/* <Route exact path="/profile/:id" component={Profile} /> */}
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <PrivateRoute exact path="/create-profile" component={ProfileForm} />
         <PrivateRoute exact path="/edit-profile" component={ProfileForm} />
