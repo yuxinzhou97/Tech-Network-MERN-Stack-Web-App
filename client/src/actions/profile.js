ec04d77629b8e9ef3f005da2d4c13fb1c8332dd4
Yuxin Zhou <yuxinzhou1997@gmail.com>
2020-09-03 20:13:51 -0700

    get current users profile

---
+++ client/src/actions/profile.js
@@ -0,0 +1,30 @@
+import api from "../utils/api";
+import { setAlert } from "./alert";
+
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  CLEAR_PROFILE,
+  ACCOUNT_DELETED,
+  GET_REPOS,
+  NO_REPOS,
+} from "./types";
+
+// Get current users profile
+export const getCurrentProfile = () => async (dispatch) => {
+  try {
+    const res = await api.get("/profile/me");
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
