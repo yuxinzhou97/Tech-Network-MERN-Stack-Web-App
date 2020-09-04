ab2ca8abeeba0ed9bd3f94ac0113a561dc5dbef6
Yuxin Zhou <yuxinzhou1997@gmail.com>
2020-09-03 17:23:11 -0700

    add more reducers

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,13 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  ACCOUNT_DELETED,
+} from "../actions/types";
 
 const initialState = {
   token: localStorage.getItem("token"),
@@ -11,6 +20,14 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload,
+      };
+
     case REGISTER_SUCCESS:
       localStorage.setItem("token", payload.token);
       return {
@@ -27,7 +44,30 @@ export default function (state = initialState, action) {
         isAuthenticated: false,
         loading: false,
       };
-
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false,
+      };
+    case ACCOUNT_DELETED:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+      };
+    case AUTH_ERROR:
+    case LOGOUT:
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+      };
     default:
       return state;
   }
