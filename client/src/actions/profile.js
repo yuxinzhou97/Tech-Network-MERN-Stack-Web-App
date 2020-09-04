e049c6866109f813702f79cf07b827d80171ad97
Yuxin Zhou <yuxinzhou1997@gmail.com>
2020-09-04 16:36:20 -0700

    add get all profiles and add get profile by id

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -29,6 +29,42 @@ export const getCurrentProfile = () => async (dispatch) => {
   }
 };
 
+// Get all profiles
+export const getProfiles = () => async (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+    const res = await api.get("/profile");
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Get profile by ID
+export const getProfileById = (userId) => async (dispatch) => {
+  try {
+    const res = await api.get(`/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
 // Get Github repos
 export const getGithubRepos = (username) => async (dispatch) => {
   try {
